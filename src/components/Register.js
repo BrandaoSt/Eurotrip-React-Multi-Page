696ab46dde8b122daaa1f4c27254e929c1ad0540
Patricia Brandão <patriciabrandao7@gmail.com>
2022-03-17 15:08:00 -0300

    Configurando estados e cidades

--- src/components/Register.js
+++ src/components/Register.js
@@ -38,71 +38,95 @@ const Register = () => {
             .catch((err) => console.log(err))
     }
 
-    return (
-        <div className='register'>
-            <div className='text-center'>
-                <h1>Criar conta</h1>
-            </div>
-            <form onSubmit={addUser}>
-                <div className='align text-center mb-4 text-secondary'>
-                    <div class="form-check form-check-inline d-inline-block">
-                        <input class="form-check-input-radio" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
-                        <label class="form-check-label" for="inlineRadio1">Pessoa Física</label>
-                    </div>
-                    <div class="form-check form-check-inline d-inline-block">
-                        <input class="form-check-input-radio" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
-                        <label class="form-check-label" for="inlineRadio2">Pessoa Jurídica</label>
-                    </div>
-                </div>
-                <div className='row justify-content-center'>
-                    <div className='col-4'>
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Nome" ref={email} />
-                            <div className='line'></div>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Email" ref={email} />
-                            <div className='line'></div>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="CPF ou CNPJ" ref={cpfOuCnpj} />
-                            <div className='line'></div>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Telefone" ref={telefone1} />
-                            <div className='line'></div>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Logradouro" ref={logradouro} />
-                            <div className='line'></div>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Número" ref={numero} />
-                            <div className='line'></div>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Complemento" ref={complemento} />
-                            <div className='line'></div>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Bairro" ref={bairro} />
-                            <div className='line'></div>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" className="form-control" placeholder="CEP" ref={cep} />
-                            <div className='line'></div>
-                        </div>
-                        <div className="form-group">
-                            <input type="password" className="form-control" placeholder="Senha" ref={senha} />
-                            <div className='line'></div>
-                        </div>
+    function getEstados(event) {
+        event.preventDefault()
+        Api.get('/estados', {
+            estadosId: estadoId.current.value
+        }).then((res) => console.log(res))
+            .catch((err) => console.log(err))
+    }
 
-                        <button className='btn-forgot forgot-btn' type="submit">Enviar</button>
-                    </div>
-                </div>
-            </form>
+    function getCidades(event) {
+        event.preventDefault()
+        Api.get('/estados/2/cidades', {
+            cidadeId: cidadeId.current.value
+        }).then((res) => console.log(res))
+            .catch((err) => console.log(err))
+    }
+
+return (
+    <div className='register'>
+        <div className='text-center'>
+            <h1>Criar conta</h1>
         </div>
-    )
+        <form onSubmit={addUser}>
+            <div className='align text-center mb-4 text-secondary'>
+                <div className="form-check form-check-inline d-inline-block">
+                    <input className="form-check-input-radio" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+                    <label className="form-check-label" for="inlineRadio1">Pessoa Física</label>
+                </div>
+                <div className="form-check form-check-inline d-inline-block">
+                    <input className="form-check-input-radio" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                    <label className="form-check-label" for="inlineRadio2">Pessoa Jurídica</label>
+                </div>
+            </div>
+            <div className='row justify-content-center'>
+                <div className='col-4'>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Nome" ref={email} />
+                        <div className='line'></div>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Email" ref={email} />
+                        <div className='line'></div>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="CPF ou CNPJ" ref={cpfOuCnpj} />
+                        <div className='line'></div>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Telefone" ref={telefone1} />
+                        <div className='line'></div>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Logradouro" ref={logradouro} />
+                        <div className='line'></div>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Número" ref={numero} />
+                        <div className='line'></div>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Complemento" ref={complemento} />
+                        <div className='line'></div>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Bairro" ref={bairro} />
+                        <div className='line'></div>
+                    </div>
+                    <select onSubmit={getEstados} className="form-select" aria-label="Estado" ref={estadoId}>
+                        <option selected>Estado</option>
+                        <option value="estadoId">{estadoId.nome}</option>
+                    </select>
+                    <select onSubmit={getCidades} className="form-select" aria-label="Cidade" ref={cidadeId}>
+                        <option selected>Cidade</option>
+                        <option value="cidadeId">{cidadeId.nome}</option>
+                    </select>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="CEP" ref={cep} />
+                        <div className='line'></div>
+                    </div>
+                    <div className="form-group">
+                        <input type="password" className="form-control" placeholder="Senha" ref={senha} />
+                        <div className='line'></div>
+                    </div>
+
+                    <button className='btn-forgot forgot-btn' type="submit">Enviar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+)
 }
 
 export default Register
